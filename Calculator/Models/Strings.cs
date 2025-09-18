86ab37a377818f0333733ab0a5791078be950c8e
Liron <1002011358@educ.org.il>
2025-09-18 17:46:41 +0300

    changed string

--- Calculator/Models/Strings.cs
+++ Calculator/Models/Strings.cs
@@ -4,7 +4,7 @@
     {
         public const string FirstNumberPrompt = "Enter the first number:";
         public const string SecondNumberPrompt = "Enter the second number:";
-        public const string ResultPrompt = "The result is:";
+        public const string ResultPrompt = "Result:";
         public const string AddText = "Add";
     }
 }
